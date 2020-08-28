3eb7f02719f09ecb8385be6723c5ac3e254040f5
wangdan <1514952826@qq.com>
2020-08-28 22:19:12 +0800

    完成添加属性值静态页面

--- src/views/product/attr/list.vue
+++ src/views/product/attr/list.vue
@@ -4,25 +4,51 @@
       <CategorySelector @changeCategory="changeCategory"></CategorySelector>
     </el-card>
     <el-card style="margin-top:20px">
-      <el-button type="primary" icon="el-icon-plus" :disabled="!category3Id">添加属性</el-button>
-      <el-table border style="width: 100%" :data="attrList">
-        <el-table-column label="属性ID" width="80" align="center" type="index"></el-table-column>
-        <el-table-column label="属性名称" width="150" prop="attrName"></el-table-column>
-        <el-table-column label="属性值列表">
-          <template slot-scope="{row,$index}">
-            <el-tag
-              v-for="attrValue in row.attrValueList"
-              :key="attrValue.id"
-            >{{attrValue.valueName}}</el-tag>
-          </template>
-        </el-table-column>
-        <el-table-column label="操作" width="150">
-          <template>
-            <HintButton icon="el-icon-edit" type="warning" title="修改属性" size="mini"></HintButton>
-            <HintButton icon="el-icon-delete" type="danger" title="删除属性" size="mini"></HintButton>
-          </template>
-        </el-table-column>
-      </el-table>
+      <div v-show="isShowList">
+        <el-button
+          type="primary"
+          icon="el-icon-plus"
+          :disabled="!category3Id"
+          @click="showAddDiv"
+        >添加属性</el-button>
+        <el-table border style="width: 100%" :data="attrList">
+          <el-table-column label="属性ID" width="80" align="center" type="index"></el-table-column>
+          <el-table-column label="属性名称" width="150" prop="attrName"></el-table-column>
+          <el-table-column label="属性值列表">
+            <template slot-scope="{row,$index}">
+              <el-tag
+                v-for="attrValue in row.attrValueList"
+                :key="attrValue.id"
+              >{{attrValue.valueName}}</el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column label="操作" width="150">
+            <template>
+              <HintButton icon="el-icon-edit" type="warning" title="修改属性" size="mini"></HintButton>
+              <HintButton icon="el-icon-delete" type="danger" title="删除属性" size="mini"></HintButton>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+      <div v-show="!isShowList">
+        <el-form :inline="true" class="demo-form-inline" ref="form" :model="form">
+          <el-form-item label="属性名">
+            <el-input placeholder="请输入属性名" v-model="form.attrName"></el-input>
+          </el-form-item>
+        </el-form>
+
+        <el-button type="primary" icon="el-icon-plus" :disabled="!form.attrName">添加属性值</el-button>
+        <el-button @click="isShowList=true">取消</el-button>
+
+        <el-table border style="margin:20px 0;">
+          <el-table-column label="序号" width="80" align="center"></el-table-column>
+          <el-table-column label="属性值名称"></el-table-column>
+          <el-table-column label="操作"></el-table-column>
+        </el-table>
+
+        <el-button type="primary">保存</el-button>
+        <el-button @click="isShowList=true">取消</el-button>
+      </div>
     </el-card>
   </div>
 </template>
@@ -36,6 +62,11 @@ export default {
       category2Id: "",
       category3Id: "",
       attrList: [],
+      isShowList: true,
+      form: {
+        attrName: "",
+        attrValueList: [],
+      },
     };
   },
   methods: {
@@ -71,6 +102,11 @@ export default {
         this.attrList = result.data;
         console.log(this.attrList);
       }
+    },
+
+    // 点击添加属性按钮，显示新的页面
+    showAddDiv() {
+      this.isShowList = false;
     },
   },
 };
